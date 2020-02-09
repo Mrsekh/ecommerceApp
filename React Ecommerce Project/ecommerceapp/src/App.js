aff11a501273257dc236c0deeb693ad591743949
Mr Sekh Afrid <abhijeetmurmu97@gmail.com>
2020-02-09 18:33:13 +0530

    Devide the component into individual part , finish styling of every components

---
+++ React Ecommerce Project/ecommerceapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './pages/homepage/homepage.component'; 
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
