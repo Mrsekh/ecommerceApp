48e18c55148eeb20a8cf89d70a7b6dc283ff7b20
Mr Sekh Afrid <abhijeetmurmu97@gmail.com>
2020-02-10 21:02:43 +0530

    update our menu items to navigate with router HOC

--- React Ecommerce Project/ecommerceapp/src/App.js
+++ React Ecommerce Project/ecommerceapp/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'; 
+import {Route} from 'react-router-dom';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+
+)
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact  path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
