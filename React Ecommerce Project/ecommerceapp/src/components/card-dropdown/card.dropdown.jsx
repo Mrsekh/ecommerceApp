7460b2b0528cc4cce124da7e8758dd02216b5d81
Mr Sekh Afrid <abhijeetmurmu97@gmail.com>
2020-03-01 09:35:40 +0530

    implemented the add the items to the cart and show them in cart and grouping them as quantity

--- React Ecommerce Project/ecommerceapp/src/components/card-dropdown/card.dropdown.jsx
+++ React Ecommerce Project/ecommerceapp/src/components/card-dropdown/card.dropdown.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-Item/cart-item';
+import {connect} from 'react-redux';
 import './card.dropdown.scss';
-const CardDropDown = () => (
+const CardDropDown = ({collectionItem}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+            {
+                collectionItem.map(item => <CartItem key={item.id} item={item}/>)
+            }
             <CustomButton>GO TOCHECKOUT</CustomButton>
         </div>
     </div>
 )
-export default CardDropDown;
+const mapStateToProps = ({cart:{collectionItem}}) => ({
+    collectionItem
+})
+export default connect(mapStateToProps)(CardDropDown);
